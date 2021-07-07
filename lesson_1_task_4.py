f93e1a095c67d890f840a22ec6d1f7a176948b41
Maria <tsareva@vniitek.ru>
2021-07-07 17:13:48 +0300

    Tasks rewrited

--- lesson_1_task_4.py
+++ lesson_1_task_4.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-from pip._vendor.progress import counter
-from pipenv.vendor.orderedmultidict import f
-
-print("Задача 4.")
-a = int(input('Введите целое положительное многозначное число: '))
-if a > 0:
-    m = a % 10
-    a = a // 10
-    while a > 0:
-        if a % 10 > m:
-            m = a % 10
-            a = a // 10
-    print("Самая большая цифра в вашем числе ", m)
-=======
-
 print("Задача 4.")
 a = int(input('Введите целое положительное многозначное число: '))
 r = 0
@@ -25,6 +9,5 @@ if a > 0:
             if m > r:
                 r = m
     print("Самая большая цифра в вашем числе ", r)
->>>>>>> branch
 else:
     print("Число должно быть положительным.")
