82086e3458de955b693a132075bc4f356b6a8d47
Maria <tsareva@vniitek.ru>
2021-07-07 17:36:01 +0300

    Code rewrited

--- lesson_1_task_6.py
+++ lesson_1_task_6.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-from pip._vendor.progress import counter
-from pipenv.vendor.orderedmultidict import f
-=======
->>>>>>> branch
-=======
->>>>>>> e487f2b74c4055cb30724a8ecdf27802fb2f3629
-
 print("Задача 6.")
 print('Результативность бега.')
 first = int(input('Введите начальный результат: '))
